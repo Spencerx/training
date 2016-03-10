932fdc4b3a9fea4ca6537c7a9aae99209ca91a24
Cornelius Schumacher <cschum@suse.de>
2016-03-10 10:23:08 +0100

    Green

--- number_to_words_spec.rb
+++ number_to_words_spec.rb
@@ -3,7 +3,11 @@ require "rspec"
 require_relative "number_to_words"
 
 describe "number to words" do
-  it "converts 1 to one" do
-    expect(number_to_words(1)).to eq("one")
+  numbers = ["zero", "one", "two", "three"]
+
+  numbers.each_with_index do |word, number|
+    it "converts #{number} to #{word}" do
+      expect(number_to_words(number)).to eq(word)
+    end
   end
 end
