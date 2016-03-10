a7a02bb2801187dc2b2aef399a7cd789383d9092
Cornelius Schumacher <cschum@suse.de>
2016-03-10 13:12:20 +0100

    Last step

--- number_to_words.rb
+++ number_to_words.rb
@@ -1,4 +1,27 @@
 def number_to_words(number)
-  words = ["zero", "one", "two", "three"]
+  words = ["zero", "one", "two", "three", "four", "five", "six", "seven",
+    "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen",
+    "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+  ten_words = [ "zeroty", "ten", "twenty", "thirty", "fourty", "fifty", "sixty",
+    "seventy", "eighty", "ninety" ]
+
+  dividers = { 1000 => "thousand", 100 => "hundred" }
+
+  dividers.each do |divider, divider_name|
+    if number >= divider
+      if number % divider == 0
+        return "#{number_to_words(number/divider)} #{divider_name}"
+      end
+      return "#{number_to_words(number/divider)} #{divider_name} #{number_to_words(number%divider)}"
+    end
+  end
+
+  if number >= 20
+    if number % 10 == 0
+      return ten_words[number/10]
+    end
+    return "#{ten_words[number/10]} #{number_to_words(number%10)}"
+  end
+
   words[number]
 end
--- number_to_words_spec.rb
+++ number_to_words_spec.rb
@@ -3,7 +3,27 @@ require "rspec"
 require_relative "number_to_words"
 
 describe "number to words" do
-  numbers = { 0 => "zero", 1 => "one", 2 => "two", 3 => "three" }
+  numbers = { 0 => "zero", 1 => "one", 2 => "two", 3 => "three" , 4 => "four",
+    5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine",
+    10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen",
+    14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen",
+    18 => "eighteen", 19 => "nineteen", 20 => "twenty", 21 => "twenty one",
+    22 => "twenty two", 23 => "twenty three", 29 => "twenty nine",
+    30 => "thirty", 35 => "thirty five", 40 => "fourty", 41 => "fourty one",
+    90 => "ninety", 100 => "one hundred", 200 => "two hundred",
+    101 => "one hundred one", 202 => "two hundred two",
+    205 => "two hundred five", 210 => "two hundred ten",
+    247 => "two hundred fourty seven",
+    999 => "nine hundred ninety nine",
+    1000 => "one thousand", 1005 => "one thousand five",
+    1007 => "one thousand seven",
+    2000 => "two thousand", 2001 => "two thousand one",
+    3000 => "three thousand", 3100 => "three thousand one hundred",
+    9999 => "nine thousand nine hundred ninety nine",
+    10000 => "ten thousand",
+    99999 => "ninety nine thousand nine hundred ninety nine",
+    100001 => "one hundred thousand one",
+    1000000 => "one million", 2743634 => "two million seven hundred fourty three thousand six hundred thirty four" }
 
   numbers.each do |number, word|
     it "converts #{number} to #{word}" do
